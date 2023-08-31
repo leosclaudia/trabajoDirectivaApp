17270b3309eb9192dd3c11e0f54ffe91ed7bd231
leosclaudia <yoleosclaudia@gmail.com>
2023-08-31 02:58:19 -0300

    registro de usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  titulo= 'Registro de Usuarios';
+  mensaje=""
+  registrado=false;
+  nombre:string="";
+  apellido:string="";
+  entradas:Array<any>;
+
+  constructor(){
+
+    this.entradas=[
+
+      {titulo:"Pyton Cada día mas presente"},
+      {titulo:"Java presente desde hace 20 años"},
+      {titulo:"JavaScript cada ves más funcional"},
+      {titulo:"Kotin potencia par atus apps"},
+      {titulo:"¡Dónde quedó pascal"},
+    ]
+    
+  }
+  registrarUsuario(){
+    this.registrado=true
+
+    this.mensaje="Usuario Registrado con éxito"
+  }
 }
